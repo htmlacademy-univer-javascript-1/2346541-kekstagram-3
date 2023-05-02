e96328f89fa3e57ff8b180fbfff2fa13f0c57e74
Chen <chenaoxuanviva@gmail.com>
2023-05-02 14:46:37 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const getRandom = function(theMin,theMax){
+  if(theMin < 0 || theMax <= theMin){
+    console.error("диапазон может быть только положительный, значение «theMin»необхотимо меньшее, чем значение «theMax»")
+    return 0;
+  }
+  return theMax + Math.random() * (theMax - theMin);
+}
+const checkLengthString = function(textString,lengthInput){
+  return textString.length >=  lengthInput;
+}

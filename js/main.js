abdd1a35f5eb7f809f9c3091909daceb5afdaa9b
Chen <chenaoxuanviva@gmail.com>
2023-05-02 15:05:19 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const getRandom = function(theMin,theMax){
   if(theMin < 0 || theMax <= theMin){
     return 0;
-  };
+  }
   return theMax + Math.random() * (theMax - theMin);
 };
 const checkLengthString = function(textString,lengthInput){
   return textString.length >=  lengthInput;
 };
+console.log(getRandom(2,10));
+console.log(checkLengthString('abcd',2));

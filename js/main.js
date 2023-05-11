15d6354edb9ae8485ccd6705cf72b2c8a3512c0b
Chen <chenaoxuanviva@gmail.com>
2023-05-11 20:53:20 +0300

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 import {creatArray} from './data.js';
+const picturesToAdd = document.querySelector('.pictures');
+const template = document.querySelector('#picture');
+const content = template.content;
+const documentFlagment = document.createDocumentFragment();
 const testArray = creatArray();
+for(let i = 0; i < testArray.length;i++){
+  const copyForInput = content.cloneNode(true);
+  copyForInput.querySelector('.picture__img').src = testArray[i].url;
+  copyForInput.querySelector('.picture__likes').textContent = testArray[i].like;
+  copyForInput.querySelector('.picture__comments').textContent = testArray[i].comments;
+  documentFlagment.append(copyForInput);
+}
 // eslint-disable-next-line no-console
-console.log(testArray);
+console.log(documentFlagment);
+//to test the number and the out put of pictures
+picturesToAdd.appendChild(documentFlagment);
+
 
